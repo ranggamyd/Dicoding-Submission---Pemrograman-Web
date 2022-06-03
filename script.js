8af9594048671ca1ae719ec76ff6c23a0a3aefe6
Rangga Manggala <ranggaamyd@gmail.com>
2022-06-03 17:46:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// menambahkan kelas responsif untuk menampilkan dropdown menu pada mobile device
+function showNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
